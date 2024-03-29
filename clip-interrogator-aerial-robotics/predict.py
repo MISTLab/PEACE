6e1e4c0718767b6c68e11cc36ac43a3cf62b2443
Haechan Mark Bong <haechan.bong@gmail.com>
2024-03-29 14:29:09 -0400

    Update predict.py

--- clip-interrogator-aerial-robotics/predict.py
+++ clip-interrogator-aerial-robotics/predict.py
@@ -1,31 +1,29 @@
 import sys
 from PIL import Image
 from cog import BasePredictor, Input, Path
-
 from clip_interrogator import Interrogator, Config
-
 
 class Predictor(BasePredictor):
     def setup(self):
         self.ci = Interrogator(Config(
-            blip_model_url='cache/model_large_caption.pth',
-            clip_model_name="ViT-L-14/openai",
-            clip_model_path='cache',
-            device='cuda:0', 
+            blip_model_url = 'cache/model_large_caption.pth',
+            clip_model_name = "ViT-L-14/openai",
+            clip_model_path = 'cache',
+            device = 'cuda:0', 
         ))
 
     def predict(
         self, 
-        image: Path = Input(description="Input image"),
+        image: Path = Input(description = "Input image"),
         clip_model_name: str = Input(
-            default="ViT-L-14/openai",
-            choices=["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"],
-            description="Choose ViT-L for CLIPSeg, and ViT-H for Stable Diffusion 2",
+            default = "ViT-L-14/openai",
+            choices = ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"],
+            description = "Choose ViT-L for CLIPSeg, and ViT-H for Stable Diffusion 2",
         ),        
         mode: str = Input(
-            default="best",
-            choices=["best", "fast"],
-            description="Prompt mode (best takes 10-20 seconds, fast takes 1-2 seconds).",
+            default = "best",
+            choices = ["best", "fast"],
+            description = "Prompt mode (best takes 10-20 seconds, fast takes 1-2 seconds).",
         ),        
     ) -> str:
         """Run a single prediction on the model"""
